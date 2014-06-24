5575b9006809eea6c0f40ad5a9937cb9d723d43e
Noam Lewis <lenoam@gmail.com>
2014-06-23 22:15:52 -0400

    fix compiler warnings in TagSoupTest

--- src-exec/TagSoupTest.hs
+++ src-exec/TagSoupTest.hs
@@ -4,19 +4,17 @@ import Text.HTML.TagSoup
 
 import Control.Exception
 import Control.Monad
-import Data.Char
 import Data.List
 import System.Cmd
 import System.Directory
 import System.Exit
 import System.IO
-import           System.Environment         (getArgs)
+import System.Environment (getArgs)
 import qualified Data.Text
-import Data.Aeson.Types
+import qualified Data.Aeson.Types as JSON
 import Data.Aeson (encode)
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Vector
-import Debug.Trace
 import qualified Data.Sequence
 import Data.Sequence  ((<|))
 import qualified Data.Foldable
@@ -26,12 +24,11 @@ strip = Data.Text.unpack . Data.Text.strip . Data.Text.pack
 
 --traceShowId a = traceShow a a
 
-
 openItem :: String -> IO String
 openItem url | not $ "http://" `isPrefixOf` url = readFile url
 openItem url = bracket
     (openTempFile "." "tagsoup.tmp")
-    (\(file,hndl) -> removeFile file)
+    (\(file,_) -> removeFile file)
     $ \(file,hndl) -> do
         hClose hndl
         putStrLn $ "Downloading: " ++ url
@@ -40,40 +37,48 @@ openItem url = bracket
         src <- readFile file
         length src `seq` return src
 
+main :: IO ()
 main = do
     args <- getArgs
     fileData <- openItem . head $ args
-    let linesWithLevels = Data.Foldable.toList . foldl  (flip convertTag) Data.Sequence.empty . parseTags $ fileData
+    let linesWithLevels = Data.Foldable.toList . parseXML $ fileData
     --let lines = map (\(level, text) -> (take (4*level) $ repeat ' ') ++ text ++ "\n") $ linesWithLevels
-    let lines = map (encode . snd) linesWithLevels
-    forM_ (reverse lines) BS.putStrLn
+    let jsonLines = map (encode . snd) linesWithLevels
+    forM_ (reverse jsonLines) BS.putStrLn
 
 -- traceJobj jobj = trace ("hi:" ++ (show . encode $ jobj)) jobj
 -- trace' x = traceStack ("yo:" ++ (show x)) x
 -- convertTag' a b = traceJobj (convertTag a b)
 
-convertTag :: Tag String -> Data.Sequence.Seq (Bool, Data.Aeson.Types.Value) -> Data.Sequence.Seq (Bool, Data.Aeson.Types.Value)
-convertTag (TagOpen name attrs) xs = (True, object (map toPair attrs)) <| xs
-    where toPair (k,v) = (Data.Text.pack k, String (Data.Text.pack v))
-convertTag (TagClose name) xs = (False, object [(Data.Text.pack name, innerObj)]) <| taill parents
+parseXML :: String -> Data.Sequence.Seq (Bool, JSON.Value)
+parseXML d = foldl (flip (\x -> convertTag $! x)) Data.Sequence.empty . parseTags $ d
+
+convertTag :: Tag String -> Data.Sequence.Seq (Bool, JSON.Value) -> Data.Sequence.Seq (Bool, JSON.Value)
+convertTag (TagOpen _ attrs) xs = (True, JSON.object (map toPair attrs)) <| xs
+    where toPair (k,v) = (Data.Text.pack k, JSON.String (Data.Text.pack v))
+convertTag (TagClose name) xs = (False, JSON.object [(Data.Text.pack name, innerObj)]) <| taill parents
     where (children, parents) = Data.Sequence.breakl fst xs
-          items = Array (Data.Vector.fromList . map snd . Data.Foldable.toList $ children)
-          attrs = snd . headl $ parents
+          items = JSON.Array (Data.Vector.fromList . map snd . Data.Foldable.toList $! children)
+          attrs = snd . headl $! parents
           innerObj = buildObject attrs items
 
 convertTag (TagText text) xs = case s of
                                    "" -> xs
-                                   _  -> (False, String (Data.Text.pack s)) <| xs
+                                   _  -> (False, JSON.String (Data.Text.pack s)) <| xs
                                where s = strip text
 convertTag _ xs = xs
 
 
+headl :: Data.Sequence.Seq a -> a
+headl s = Data.Sequence.index s 0
 
-headl seq = Data.Sequence.index seq 0
-taill seq = Data.Sequence.drop 1 seq
+taill :: Data.Sequence.Seq a -> Data.Sequence.Seq a
+taill s = Data.Sequence.drop 1 s
 
+isObject :: JSON.Value -> Bool
 isObject x = case x of 
-                 Object _ -> True
+                 JSON.Object _ -> True
                  _        -> False
 
-buildObject attrs items = object [(Data.Text.pack "attrs", attrs), (Data.Text.pack "items", items)]
+buildObject :: JSON.Value -> JSON.Value -> JSON.Value
+buildObject attrs items = JSON.object [(Data.Text.pack "attrs", attrs), (Data.Text.pack "items", items)]
